d20c2aff189586516bd56f8bba2083682f3af4de
N1cholas <z875346836@icloud.com>
2018-12-01 17:30:36 +0800

    test uplaod

--- src/main/java/com/n1cholas/service/impl/FileServiceImpl.java
+++ src/main/java/com/n1cholas/service/impl/FileServiceImpl.java
@@ -44,7 +44,7 @@ public class FileServiceImpl implements IFileService {
             FTPUtil.uploadFile(Lists.newArrayList(targetFile));
 
             //上传完成之后，删除upload下面的文件
-            targetFile.delete();
+//            targetFile.delete();
 
         } catch (IOException e) {
             logger.error("上传文件异常", e);
