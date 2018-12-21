faa7c7017afc71749f6cbc1dc1dbe67d5470ce6e
N1cholas <z875346836@icloud.com>
2018-12-21 15:21:33 +0800

    reset session expire by filter

--- src/main/java/com/n1cholas/controller/portal/UserController.java
+++ src/main/java/com/n1cholas/controller/portal/UserController.java
@@ -8,12 +8,14 @@ import com.n1cholas.service.IUserService;
 import com.n1cholas.util.CookieUtil;
 import com.n1cholas.util.JsonUtil;
 import com.n1cholas.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -45,13 +47,13 @@ public class UserController {
 
     /**
      * 用户登出
-     * @param session 用户session
-     * @return ServerResponse<String>
      */
     @RequestMapping(value = "logout.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> logout (HttpSession session) {
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        CookieUtil.delLoginToken(httpServletRequest, httpServletResponse);
+        RedisPoolUtil.del(loginToken);
         return ServerResponse.createBySuccess();
     }
 
@@ -175,17 +177,23 @@ public class UserController {
 
     /**
      * 获取用户信息
-     * @param session 用户信息
-     * @return 响应结果
      */
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser == null) {
+    public ServerResponse<User> getUserInfo(HttpServletRequest httpServletRequest) {
+        //User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtils.isEmpty(loginToken)) {
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+
+        String userJsonStr = RedisPoolUtil.get(loginToken);
+        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+
+        if (user == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "未登录，需要强制登陆");
         }
 
-        return iUserService.getUserInfo(currentUser.getId());
+        return iUserService.getUserInfo(user.getId());
     }
 }
